1b1ff581e39799e46ece7af6531787dd72b90f06
Ekaterina <Katya.Kornil10@gmail.com>
2024-02-16 15:01:15 +0300

    delete .pk models and add README

--- app/model.py
+++ app/model.py
@@ -193,8 +193,8 @@ class StyleLoss(nn.Module):
 class Normalization(nn.Module):
     def __init__(self, mean, std):
         super(Normalization, self).__init__()
-        self.mean = torch.tensor(mean).view(-1, 1, 1) # self.mean = mean.clone().detach().view(-1, 1, 1)
-        self.std = torch.tensor(std).view(-1, 1, 1) # self.std = mean.clone().detach().view(-1, 1, 1)
+        self.mean = mean.clone().detach().view(-1, 1, 1) # self.mean = torch.tensor(mean).view(-1, 1, 1) 
+        self.std = mean.clone().detach().view(-1, 1, 1) # self.std = torch.tensor(std).view(-1, 1, 1)
 
     def forward(self, img):
         return (img - self.mean) / self.std
